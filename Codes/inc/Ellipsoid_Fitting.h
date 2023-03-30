564a802a37947c8cb14e5650e1ee8d7b43d39129
Fatma52125 <fatmaalhashimi5@hotmail.com>
2023-03-31 00:18:52 +0100

    fixing files

--- Codes/inc/Ellipsoid_Fitting.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void Calculate_Fit(float v[9], float ref_radius, float off[3], float rotM[9]);
-
-void Correct_DataPoint(float XYZ[3], float off[3], float rotM[9]);
-
-void Recursive_Ellipsoid_Fit(float P[81], float theta[9], float XYZ[3], uint8_t ini, uint8_t rotation);
--- Codes/inc/Param.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void save_read_param_float(char name[], uint16_t index, float parameters[],uint16_t num, uint8_t type);
-
-void save_read_param_int16(char name[], uint16_t index, int16_t parameters[],uint16_t num, uint8_t type);
-
-void save_read_param_uint16(char name[], uint16_t index, uint16_t parameters[],uint16_t num, uint8_t type);
